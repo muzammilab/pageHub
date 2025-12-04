aa8b05bacf61e50cebe4153429af3abaf58a4f27
Bedrekar Muzammil Aslam <150377037+muzammilab@users.noreply.github.com>
2025-12-05 01:02:10 +0530

    Update Course.jsx

--- Frontend/src/Components/Course.jsx
+++ Frontend/src/Components/Course.jsx
@@ -10,7 +10,7 @@ function Course() {
   useEffect(() => {
     const getBook = async () => {
       try {
-        const res = await axios.get("https://pagehub.onrender.com/book");
+        const res = await axios.get("http://localhost:3001/book");
         console.log(res.data);
         setBook(res.data);
       } catch (error) {
