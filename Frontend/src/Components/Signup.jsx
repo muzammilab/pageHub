5914854a4587523256597420ab01ec50b616e455
Bedrekar Muzammil Aslam <150377037+muzammilab@users.noreply.github.com>
2025-12-05 00:58:00 +0530

    Updated Signup.jsx

--- Frontend/src/Components/Signup.jsx
+++ Frontend/src/Components/Signup.jsx
@@ -20,7 +20,7 @@ function Signup() {
       password: data.password,
     };
     await axios
-      .post("http://localhost:3001/user/signup", userInfo)
+      .post("https://pagehub.onrender.com/user/signup", userInfo)
       .then((res) => {
         console.log(res.data);
         if (res.data) {
